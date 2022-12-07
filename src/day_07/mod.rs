558ba2291b854ab11708bc53a6582f86c751d61c
Rodrigo Olmo <rodrigoolmo@gmail.com>
2022-12-07 17:57:55 +0100

    Fixed typo

--- src/day_07/mod.rs
+++ src/day_07/mod.rs
@@ -11,10 +11,10 @@ struct Dir {
 }
 
 fn parse_input(input: String) -> HashMap<String, Dir> {
-    let mut filesistem: HashMap<String, Dir> = HashMap::new();
+    let mut file_system: HashMap<String, Dir> = HashMap::new();
 
     let mut curr_dir_abs = "/".to_string();
-    filesistem.insert("/".to_string(), Dir {
+    file_system.insert("/".to_string(), Dir {
         father: "".to_string(),
         files: Vec::new(),
         dirs: Vec::new(),
@@ -22,7 +22,7 @@ fn parse_input(input: String) -> HashMap<String, Dir> {
     });
 
     for line in input.lines().skip(1) {
-        let father = filesistem.get(&curr_dir_abs).unwrap().father.clone();
+        let father = file_system.get(&curr_dir_abs).unwrap().father.clone();
         if line.starts_with("$ cd") {
             // Change directory
             let (_, new_dir_key_rel) = line.split_once("$ cd ").unwrap();
@@ -41,9 +41,9 @@ fn parse_input(input: String) -> HashMap<String, Dir> {
             // Folders
             let (_, new_dir_key_rel) = line.split_once("dir ").unwrap();
             let new_dir_key_abs = curr_dir_abs.clone() + "/" + new_dir_key_rel;
-            let curr_dir = filesistem.get_mut(&curr_dir_abs).unwrap();
+            let curr_dir = file_system.get_mut(&curr_dir_abs).unwrap();
             curr_dir.dirs.push(new_dir_key_abs.clone());
-            filesistem.insert(new_dir_key_abs, Dir {
+            file_system.insert(new_dir_key_abs, Dir {
                 father: curr_dir_abs.clone(),
                 files: Vec::new(),
                 dirs: Vec::new(),
@@ -53,32 +53,32 @@ fn parse_input(input: String) -> HashMap<String, Dir> {
             // Files
             let (size_str, file_name) = line.split_once(" ").unwrap();
             let size = to_i32(size_str);
-            let curr_dir = filesistem.get_mut(&curr_dir_abs).unwrap();
+            let curr_dir = file_system.get_mut(&curr_dir_abs).unwrap();
             curr_dir.files.push((file_name.to_string(), size));
         }
     }
-    filesistem
+    file_system
 }
 
-fn calculate_size(filesistem: &mut HashMap<String, Dir>, key: &String) -> i32 {
-    let dir = filesistem.get(key).unwrap().clone();
+fn calculate_size(file_system: &mut HashMap<String, Dir>, key: &String) -> i32 {
+    let dir = file_system.get(key).unwrap().clone();
     let size_files: i32 = dir.files.iter()
         .map(|(_f, size)| size)
         .sum();
     let mut size_folders = 0;
     for folder in &dir.dirs.clone() {
-        size_folders += calculate_size(filesistem, folder);
+        size_folders += calculate_size(file_system, folder);
     } 
     let total_size = size_files + size_folders;
-    filesistem.entry(key.clone()).and_modify(|e| e.size = Some(total_size));
+    file_system.entry(key.clone()).and_modify(|e| e.size = Some(total_size));
     total_size
 }
 
 pub fn part1(input: String) -> String {
-    let mut filesistem = parse_input(input);
-    calculate_size(&mut filesistem, &"/".to_string());
+    let mut file_system = parse_input(input);
+    calculate_size(&mut file_system, &"/".to_string());
 
-    let result: i32 = filesistem.iter()
+    let result: i32 = file_system.iter()
         .map(|(_k, dir)| dir.size.unwrap())
         .filter(|&size| size <= 100_000).sum();
 
@@ -86,13 +86,13 @@ pub fn part1(input: String) -> String {
 }
 
 pub fn part2(input: String) -> String {
-    let mut filesistem = parse_input(input);
-    calculate_size(&mut filesistem, &"/".to_string());
+    let mut file_system = parse_input(input);
+    calculate_size(&mut file_system, &"/".to_string());
 
-    let size_total = filesistem.get(&"/".to_string()).unwrap().size.unwrap();
+    let size_total = file_system.get(&"/".to_string()).unwrap().size.unwrap();
     let space_to_free = 30_000_000 - (70_000_000 - size_total);
 
-    let result = filesistem.iter()
+    let result = file_system.iter()
         .map(|(_k, v)|v.size.unwrap())
         .filter(|&size| size > space_to_free).min().unwrap();
 
